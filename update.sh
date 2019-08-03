aab7d5816a1ffc2373f6fcbf3ccd99976a73c683
Le Cong Hau <anhlahau.hl@gmail.com>
2019-08-03 10:05:15 +0700

    checkout master before pull

--- update.sh
+++ update.sh
@@ -3,37 +3,37 @@
 echo
 echo
 echo ---------------UPDATE DOCS----------------
-cd ./docs && git pull origin master;
+cd ./docs && git checkout master && git pull origin master;
 echo
 echo
 echo ---------------UPDATE MODEL-PREPROCESS----------------
 cd ..;
-cd ./model-preprocess && git pull origin master;
+cd ./model-preprocess && git checkout master && git pull origin master;
 echo
 echo
 echo ---------------UPDATE MODEL-CREATE----------------
 cd ..;
-cd ./model-create && git pull origin master;
+cd ./model-create && git checkout master && git pull origin master;
 echo
 echo
 echo ---------------UPDATE MODEL-DEPLOY----------------
 cd ..;
-cd ./model-deploy && git pull origin master;
+cd ./model-deploy && git checkout master && git pull origin master;
 echo
 echo
 echo ---------------UPDATE EDGE-SERVER----------------
 cd ..;
-cd ./edge-server && git pull origin master;
+cd ./edge-server && git checkout master && git pull origin master;
 echo
 echo
 echo ---------------UPDATE CLOUD-SERVER----------------
 cd ..;
-cd ./cloud-server && git pull origin master;
+cd ./cloud-server && git checkout master && git pull origin master;
 echo
 echo
 echo ---------------UPDATE FRONTEND----------------
 cd ..;
-cd ./cloud-frontend && git pull origin master;
+cd ./cloud-frontend && git checkout master && git pull origin master;
 cd ..;
 echo
 read -p "Copy docs/docker-compose.yml to ./docker-compose.yml? (y/n)" -n 1 -r
