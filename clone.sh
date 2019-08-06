b6e09633de7801b173ae99f9e665b89101824bb6
Quang Dang <42106752+Homuru@users.noreply.github.com>
2019-08-06 12:10:46 +0700

    Added clone.sh

---
+++ clone.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo
+echo
+echo ---------------Clone DOCS----------------
+git clone https://github.com/bti-training-20183/docs
+echo
+echo
+echo ---------------Clone MODEL-PREPROCESS----------------
+git clone https://github.com/bti-training-20183/model-preprocess
+echo
+echo
+echo ---------------Clone MODEL-CREATE----------------
+git clone https://github.com/bti-training-20183/model-create
+echo
+echo
+echo ---------------Clone MODEL-DEPLOY----------------
+git clone https://github.com/bti-training-20183/model-deploy
+echo
+echo
+echo ---------------Clone EDGE-SERVER----------------
+git clone https://github.com/bti-training-20183/edge-server
+echo
+echo
+echo ---------------Clone CLOUD-SERVER----------------
+git clone https://github.com/bti-training-20183/cloud-server
+echo
+echo
+echo ---------------Clone FRONTEND----------------
+git clone https://github.com/bti-training-20183/cloud-frontend
+echo
+read -p "Copy docs/docker-compose.yml to ./docker-compose.yml? (y/n)" -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cp ./docs/docker-compose.yml .
+    echo "Copied docs/docker-compose.yml to ./docker-compose.yml"
+fi
